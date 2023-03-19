685929fedd3782fdd49928c90034e9e8ad8f4e94
mayankgupta804 <mayank.gupta804@gmail.com>
2023-03-19 13:41:35 +0530

    Add new method to database to fetch all values in a bucket

--- pkg/database/bbolt.go
+++ pkg/database/bbolt.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"errors"
 	"time"
 
 	bolt "go.etcd.io/bbolt"
@@ -34,8 +35,35 @@ func (db *DB) Get(schema, key []byte) []byte {
 	var val []byte
 	db.client.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(schema)
+		if b == nil {
+			return errors.New("bucket does not exist")
+		}
 		val = b.Get(key)
 		return nil
 	})
 	return val
+}
+
+func (db *DB) GetAll(schema []byte) [][]byte {
+	vals := make([][]byte, 0)
+	db.client.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(schema)
+		if b == nil {
+			return errors.New("bucket does not exist")
+		}
+		c := b.Cursor()
+		_, val := c.First()
+		if val == nil {
+			return nil
+		}
+		vals = append(vals, val)
+		for {
+			_, v := c.Next()
+			if v == nil {
+				return nil
+			}
+			vals = append(vals, v)
+		}
+	})
+	return vals
 }

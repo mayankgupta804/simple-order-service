9343a60c54664faf0c8787462d30d442ec0b2c98
mayankgupta804 <mayank.gupta804@gmail.com>
2023-03-28 17:08:24 +0530

    Add basic test to create an order with two products

--- internal/domain/order_test.go
+++ internal/domain/order_test.go
@@ -6,31 +6,39 @@ import (
 )
 
 func TestCreateOrderWithProducts(t *testing.T) {
-	order := domain.NewOrder("123")
-	if order.Products() == nil {
-		t.Fail()
+	orderID := "123"
+
+	order := domain.NewOrder(orderID)
+	if len(order.Products()) != 0 {
+		t.Error("products in the order must be empty when a new order is created")
 	}
 	if order.ProductToCount() == nil {
-		t.Fail()
-	}
-	if order.ID() != "123" {
-		t.Fail()
+		t.Error("the map of products in the order to the count must be initialized")
 	}
 
 	product1 := domain.NewProduct("1", "nike shoes", 11.0, 3, domain.Premium)
 	product2 := domain.NewProduct("2", "adidas shoes", 13.0, 2, domain.Premium)
+
 	order.Add(product1)
 	order.Add(product2)
 
-	if order.Value() != 35.0 {
-		t.Fail()
+	expectedOrderValue := product1.Price() + product2.Price()
+	actualOrderValue := order.Value()
+
+	if actualOrderValue != expectedOrderValue {
+		t.Errorf("Expected order value: %v. Got order value: %v", expectedOrderValue, actualOrderValue)
 	}
 
-	if order.ProductToCount()["1"] != 2 {
-		t.Fail()
+	actualProduct1Count := order.ProductToCount()["1"]
+	expectedProduct1Count := 1
+
+	if actualProduct1Count != expectedProduct1Count {
+		t.Errorf("Expected count of product1: %d. Got count of product1: %d", expectedProduct1Count, actualProduct1Count)
 	}
 
-	if len(order.Products()) != 3 {
-		t.Fail()
+	expectedTotalOrderedProducts := 2
+	actualTotalOrderedProducts := len(order.Products())
+	if expectedTotalOrderedProducts != actualTotalOrderedProducts {
+		t.Errorf("Expected total products: %d. Got total products: %d", expectedProduct1Count, actualProduct1Count)
 	}
 }
